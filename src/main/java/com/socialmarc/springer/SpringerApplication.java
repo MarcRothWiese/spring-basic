e33578e57929cb262fe304d0a5569d000bea7d8f
Marc Wiese <marc@wiese.xyz>
2020-12-04 17:33:50 +0100

    Adding

--- src/main/java/com/socialmarc/springer/SpringerApplication.java
+++ src/main/java/com/socialmarc/springer/SpringerApplication.java
@@ -2,8 +2,6 @@ package com.socialmarc.springer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class SpringerApplication {
@@ -12,8 +10,4 @@ public class SpringerApplication {
         SpringApplication.run(SpringerApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
 }
--- src/main/java/com/socialmarc/springer/controller/HomeController.java
+++ src/main/java/com/socialmarc/springer/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.socialmarc.springer.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +13,14 @@ public class HomeController {
         return "Hello World!";
     }
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello There";
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
+
+    @GetMapping("/error")
+    public String sayError() {
+        return "Error happend";
+    }
+
 }
